3e631d086cdf1e16e2dfaee208c934f86ad862ac
slamflipstrom <lindstrom.s@gmail.com>
2014-10-01 23:56:20 -0500

    added user_id to listings to solidify Active Record association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :password_digest
   has_many :listings
   
   # has_secure_password
