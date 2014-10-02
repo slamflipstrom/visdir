ea302fd764b286c1c9238d63d9c7164f951827d1
slamflipstrom <lindstrom.s@gmail.com>
2014-10-02 13:07:22 -0500

    Added boolean to users indicating pro or non-pro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141002045429) do
+ActiveRecord::Schema.define(:version => 20141002180626) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20141002045429) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.boolean  "pro"
   end
 
 end
