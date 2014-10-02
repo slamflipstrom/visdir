7f7366dc909d3ff7450ee4619b3a288f7bee2c25
slamflipstrom <lindstrom.s@gmail.com>
2014-10-02 11:55:22 -0500

    Added Sumeet's CSS & Javascript

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-  attr_accessible :budget_type, :category_id, :location, :name
+  attr_accessible :budget_type, :category_id, :location, :name, :user_id
   belongs_to :users
   belongs_to :categories
   
