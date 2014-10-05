728e283725c21dffdedf1f96d4e05fdace302a09
slamflipstrom <lindstrom.s@gmail.com>
2014-10-05 14:02:46 -0500

    current_user & authorize methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+  before_filter :authorize
+  
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+  
+  def authorize
+    if current_user.nil?
+      redirect_to login_path, :alert => "You need to log in to do that."
+    end
+  end
+
+
 end
